c1d90dd482e4d934183fcd90b0fbf23df3e6786a
xAm64 <48089283+xAm64@users.noreply.github.com>
2025-01-31 14:59:54 +0100

    Banière haut
    Mise a jour logo.
    Heure
    Barre de recherche
    Icon de connexion

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*met heure en temps réel toutes les 15 secondes. */
+function updateTime() {
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let currentTime = `${hours}:${minutes}`;
+    document.getElementById('current-time').textContent = currentTime;
+}
+setInterval(updateTime, 15000);
+updateTime();
