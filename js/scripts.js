f946adecc545076c6459991bff9b4f53bc251b40
xAm64 <48089283+xAm64@users.noreply.github.com>
2025-01-31 15:25:58 +0100

    Main et footer

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ function updateTime() {
     let hours = now.getHours().toString().padStart(2, '0');
     let minutes = now.getMinutes().toString().padStart(2, '0');
     let currentTime = `${hours}:${minutes}`;
+    let year = now.getFullYear();
     document.getElementById('current-time').textContent = currentTime;
+    document.getElementById("get-current-year").textContent = year;
 }
 setInterval(updateTime, 15000);
 updateTime();
