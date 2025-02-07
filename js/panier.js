b9cfa67dc37ee6941afca8abcd985e80d3579b34
xAm64 <48089283+xAm64@users.noreply.github.com>
2025-02-07 15:51:53 +0100

    Gestion du formulaire Partie 1
    On peut désormait avoir une adresse de facturation et de livraison différente.
    La gestion du contenue est faite.
    Le design des checkbox aussi.

--- js/panier.js
+++ js/panier.js
@@ -94,8 +94,12 @@ function acheter(valeur){
 
 function paiement(accept) {
     if (accept == true) {
-        alert("Merci d'avoir complété la simulation de commande ! Nous espérons que vous avez apprécié l'expérience.");
-        clearCartAndForm();
+        if (activerPaiement()){
+            alert("Merci d'avoir complété la simulation de commande ! Nous espérons que vous avez apprécié l'expérience.");
+            clearCartAndForm();
+        } else {
+            alert("Veuillez remplir les champs obligatoires manquants avant de valider votre commande !");
+        }
     } else {
         alert("Votre demande d'annulation a bien été prise en compte !");
         clearCartAndForm();
@@ -107,4 +111,82 @@ function clearCartAndForm() {
     localStorage.setItem("panier", JSON.stringify(panier));
     displayCart();
     document.querySelectorAll('#paiement form input').forEach(input => input.value = '');
+}
+
+function toggleDeliveryAddress() {
+    const adresseLivraison = document.getElementById("adresse-livraison");
+    const checkbox = document.getElementById("facturation-is-diferent");
+    const livraisonAndFacturation = document.getElementById("adresse-factur");
+    if (checkbox.checked) {
+        adresseLivraison.style.display = "block";
+        adresseLivraison.innerHTML = `
+        <h5>Adresse de livraison</h5>
+        <div class="container">
+            <div class="group-vertical">
+                <label for="nom">Nom<span class="danger">*</span></label>
+                <input type="text" id="livraison-nom" name="livraison-nom" placeholder="Smith" required>
+            </div>
+            <div class="group-vertical">
+                <label for="prenom">Prénom<span class="danger">*</span></label>
+                <input type="text" id="livraison-prenom" name="livraison-prenom" placeholder="John" required>
+            </div>
+        </div>
+        <div class="container">
+            <div class="group-vertical">
+                <label for="adresse">Adresse<span class="danger">*</span></label>
+                <input type="text" id="livraison-adresse" name="livraison-adresse" placeholder="1 rue de Paris" required>
+            </div>
+            <div class="group-vertical">
+                <label for="complement">Complément</label>
+                <input type="text" id="livraison-complement" name="livraison-complement" placeholder="Appartement 123">
+            </div>
+        </div>
+        <div class="container">
+            <div class="group-vertical">
+                <label for="code-portal">Code postal<span class="danger">*</span></label>
+                <input type="text" id="livraison-code-portal" name="livraison-code-portal" placeholder="75000" required>
+            </div>
+            <div class="group-vertical">
+                <label for="ville">Ville<span class="danger">*</span></label>
+                <input type="text" id="livraison-ville" name="livraison-ville" placeholder="Paris" required>
+            </div>
+        </div>
+        `;
+        livraisonAndFacturation.innerText = "Adresse de facturation";
+    } else {
+        adresseLivraison.style.display = "none";
+        document.getElementById("adresse-livraison").innerHTML = "";
+        livraisonAndFacturation.innerText = "Adresse de facturation et de livraison";
+    }
+}
+
+/* va tout controler avant d'activer le paiement */
+function activerPaiement(){
+    let etat = true;
+    let listIdentifiants = ["email", "facturation-nom", "facturation-prenom", "facturation-adresse", "facturation-code-portal", "facturation-ville", "cb-titulaire", "cb-numero", "cb-expire", "cb-code"];
+    let listLivraison = ["livraison-nom", "livraison-prenom", "livraison-adresse", "livraison-code-portal", "livraison-ville"];
+    const checkboxLivraisonDifferente = document.getElementById("facturation-is-diferent");
+    for (let i=0; i<listIdentifiants.length; i++){
+        if (!verifEtat(listIdentifiants[i])){
+            etat = false;
+        }
+    }
+    if(checkboxLivraisonDifferente.checked){
+        for (let i = 0; i < listLivraison.length; i++){
+            if(!verifEtat(listLivraison[i])){
+                etat = false;
+            }
+        }
+    }
+    return etat;
+}
+
+function verifEtat(id){
+    if (document.getElementById(id).value != ""){
+        document.getElementById(id).style = "border: 1px solid #818274";
+        return true;
+    }else{
+        document.getElementById(id).style = "border: 1px solid #db4f2c";
+        return false;
+    }
 }
